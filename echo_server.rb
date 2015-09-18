8d69248f74698a9877ec84b51053b88a5223113b
Joshua Cook <jcook@invoca.com>
2015-09-18 08:52:17 -0700

    added handling

--- echo_server.rb
+++ echo_server.rb
@@ -7,34 +7,11 @@ post '/direct' do
   { :destination_phone_number => destination_phone_number }.to_json
 end
 
-get '/direct' do
-  destination_phone_numbers = ['+13104331646','+18004377950']
-  destination_phone_number = destination_phone_numbers[rand(2)]
-  content_type :json
-  { :destination_phone_number => destination_phone_number }.to_json
-end
-
-get '/promo' do
-  destination_phone_numbers = ['+13104331646','+18004377950']
-  destination_phone_number = destination_phone_numbers[rand(2)]
-  content_type :json
-  { :destination_phone_number => destination_phone_number }.to_json
-end
-
 post '/promo' do
   destination_phone_numbers = ['+13104331646','+18004377950']
   destination_phone_number = destination_phone_numbers[rand(2)]
   content_type :json
   { :destination_phone_number => destination_phone_number }.to_json
-end
-
-get '/bundle' do
-  request.body.rewind
-  request_payload = JSON.parse request.body.read
-  destination_campaign_ids = ['80','81','79','34','32']
-  destination_campaign_id = destination_campaign_ids[rand(5)]
-  content_type :json
-  { :destination_campaign_id => destination_campaign_id}.to_json
 end
 
 post '/bundle' do
