43ade63a856984cb574fe0dc96bc36a397a5d62a
Joshua Cook <jcook@invoca.com>
2015-09-17 23:32:43 -0700

    json

--- echo_server.rb
+++ echo_server.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'json'
 get '/' do
-  { destination_phone_number: "+13104331646"}, {destination_phone_number: "+18004377950"} 
+  content_type :json
+  { :destination_phone_number => "+13104331646"}, {:destination_phone_number => "+18004377950"}.to_json 
 end
