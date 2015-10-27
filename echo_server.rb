965608f1539b1b4cd7eba2d4481d2456a083a36c
Joshua Cook <jcook@invoca.com>
2015-10-27 16:51:18 -0700

    update integration code

--- echo_server.rb
+++ echo_server.rb
@@ -23,6 +23,9 @@ post '/bundle' do
   { :destination_campaign_id => destination_campaign_id}.to_json
 end
 
+get '/' do
+  erb :index
+end
 
 post '/bundificate' do
   request.body.rewind
@@ -33,3 +36,28 @@ post '/bundificate' do
   { :destination_campaign_id_from_network => destination_campaign_id}.to_json
 end
 
+__END__
+@@layout
+<% title="Advertiser landing page" %>
+<!doctype html>
+<html lang="en">
+<head>
+  <title><%= title %></title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <header>
+    <h1><%= title %></h1>
+       <h2>Call now: <span>555-555-1234</span></h2>
+    <!-- Begin Call Tracking Code -->
+    <!-- Omit http from path to ensure protocol is same as current request -->
+    <script src="//sfapp.testring.com/10/integration.js"></script>
+    <script type="text/javascript">
+     Invoca.advertiser_integration = {
+      id : '26'
+     };
+    </script>
+    <!-- End Call Tracking Code -->
+  </header>
+</body>
+</html>
