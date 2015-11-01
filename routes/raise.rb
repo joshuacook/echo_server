cec57bb56b4c21b65b9b2dc78fd2b3e12d01ea20
Joshua Cook <jcook@invoca.com>
2015-11-01 14:50:08 -0800

    change param to pass test

--- routes/raise.rb
+++ routes/raise.rb
@@ -3,6 +3,10 @@ class MyApp < Sinatra::Application
   post '/raise' do
     request.body.rewind
     input = request.body.read    
-    input
+    input["conversion"]
   end
 end
+
+
+
+# curl -H "Content-Type: application/json" -XPOST -d '{"kind"=>"doubleclicksearch#conversionList", "conversion"=>[{"clickId"=>"badclid_two", "conversionTimestamp"=>"1446415352900", "conversionId"=>"F77FE112-EA3ADD76", "segmentationType"=>"FLOODLIGHT", "segmentationName"=>"offline_calls_trans", "type"=>"TRANSACTION", "revenueMicros"=>"1000000", "currencyCode"=>"USD"}]}' http://dry-castle-8995.herokuapp.com/raise
