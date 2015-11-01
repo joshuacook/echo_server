255540390a22c19b230fe11770fac8d077ab35ac
Joshua Cook <jcook@invoca.com>
2015-11-01 11:12:23 -0800

    params for raise

--- routes/raise.rb
+++ routes/raise.rb
@@ -2,9 +2,12 @@
 class MyApp < Sinatra::Application
   post '/raise' do
     id = params[:id]    
-    status 400 unless id="foo"
-    status 200 if id="foo"
-    body "Fail" unless id = "foo"
-    body "Success. #{id} \n" if id="foo"
+    if id = "foo"
+      status 200 if id="foo"
+      body "Success. #{id} \n" if id="foo"
+    else
+      status 400 unless id="foo"
+      body "Fail" unless id = "foo"
+    end
   end
 end
