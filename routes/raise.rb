4781d4b0a978c216c18ac132c94bddf5cd7be3ea
Joshua Cook <jcook@invoca.com>
2015-11-01 19:49:19 -0800

    change param to pass test

--- routes/raise.rb
+++ routes/raise.rb
@@ -4,7 +4,10 @@ class MyApp < Sinatra::Application
     request.body.rewind
     input = JSON.parse request.body.read.gsub('=>', ':')
     logger.debug "Handling 'doubleclick' request."
-    if input['conversion'][0]['clickId'] == 'goodclid_two'
+    if input['conversion'][0]['clickId'] == 'bad'
+      status 200
+      body "Success."
+    elsif input['conversion'][0]['clickId'] == 'bad'
       status 200
       body "Success."
     else
