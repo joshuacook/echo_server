507addf5160273ba06ae789145dd518006d3cf90
Joshua Cook <jcook@invoca.com>
2015-12-08 13:35:51 -0800

    added logging

--- routes/real_time_routing.rb
+++ routes/real_time_routing.rb
@@ -4,27 +4,32 @@ class MyApp < Sinatra::Application
   get '/real_time_routing' do
     slim :real_time_routing
   end
-  
+
   post '/real_time_routing/bundle' do
     request.body.rewind
     request_payload = JSON.parse request.body.read
+    logger.debug request_payload
     destination_campaign_ids = request_payload['ids']
     logger.debug destination_campaign_ids
     destination_campaign_id = destination_campaign_ids.sample
     content_type :json
     { :destination_campaign_id => destination_campaign_id}.to_json
   end
-  
+
   post '/real_time_routing/bundificate' do
     request.body.rewind
     request_payload = JSON.parse request.body.read
+    logger.debug request_payload
     destination_campaign_ids = request_payload['ids']
     destination_campaign_id = destination_campaign_ids.sample
     content_type :json
     { :destination_campaign_id_from_network => destination_campaign_id}.to_json
   end
-  
+
   post '/real_time_routing/promo' do
+    request.body.rewind
+    request_payload = JSON.parse request.body.read
+    logger.debug request_payload
     destination_phone_numbers = ['+17074437062','+19072665145','+15132411010','+12076883210']
     destination_phone_number = destination_phone_numbers.sample
     content_type :json
@@ -32,6 +37,9 @@ class MyApp < Sinatra::Application
   end
 
   post '/real_time_routing/extensions' do
+    request.body.rewind
+    request_payload = JSON.parse request.body.read
+    logger.debug request_payload
     destination_phone_number = '+18554474133'
     destination_phone_number = destination_phone_number + 'x,,,,' + 1.to_s
     content_type :json
