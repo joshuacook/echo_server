33bd77b5884599f8232eb34922bf27be3be28bdb
Joshua Cook <jcook@invoca.com>
2015-12-17 11:39:47 -0800

    preparing to update for multiple format rtr

--- routes/foo.rb
+++ routes/foo.rb
@@ -1,29 +1,29 @@
 post '/foo' do
   t = %w[text/css text/html application/javascript]
-  request.accept              # ['text/html', '*/*']
-  request.accept? 'text/xml'  # true
-  request.preferred_type(t)   # 'text/html'
-  request.body                # request body sent by the client (see below)
-  request.scheme              # "http"
-  request.script_name         # "/example"
-  request.path_info           # "/foo"
-  request.port                # 80
-  request.request_method      # "GET"
-  request.query_string        # ""
-  request.content_length      # length of request.body
-  request.media_type          # media type of request.body
-  request.host                # "example.com"
-  request.post?                # true (similar methods for other verbs)
-  request.form_data?          # false
-  request["some_param"]       # value of some_param parameter. [] is a shortcut to the params hash.
-  request.referrer            # the referrer of the client or '/'
-  request.user_agent          # user agent (used by :agent condition)
-  request.cookies             # hash of browser cookies
-  request.xhr?                # is this an ajax request?
-  request.url                 # "http://example.com/example/foo"
-  request.path                # "/example/foo"
-  request.ip                  # client IP address
-  request.secure?             # false (would be true over ssl)
-  request.forwarded?          # true (if running behind a reverse proxy)
-  request.env                 # raw env hash handed in by Rack
+logger.debug   request.accept              # ['text/html', '*/*']
+logger.debug   request.accept? 'text/xml'  # true
+logger.debug   request.preferred_type(t)   # 'text/html'
+logger.debug   request.body                # request body sent by the client (see below)
+logger.debug   request.scheme              # "http"
+logger.debug   request.script_name         # "/example"
+logger.debug   request.path_info           # "/foo"
+logger.debug   request.port                # 80
+logger.debug   request.request_method      # "GET"
+logger.debug   request.query_string        # ""
+logger.debug   request.content_length      # length of request.body
+logger.debug   request.media_type          # media type of request.body
+logger.debug   request.host                # "example.com"
+logger.debug   request.post?                # true (similar methods for other verbs)
+logger.debug   request.form_data?          # false
+logger.debug   request["some_param"]       # value of some_param parameter. [] is a shortcut to the params hash.
+logger.debug   request.referrer            # the referrer of the client or '/'
+logger.debug   request.user_agent          # user agent (used by :agent condition)
+logger.debug   request.cookies             # hash of browser cookies
+logger.debug   request.xhr?                # is this an ajax request?
+logger.debug   request.url                 # "http://example.com/example/foo"
+logger.debug   request.path                # "/example/foo"
+logger.debug   request.ip                  # client IP address
+logger.debug   request.secure?             # false (would be true over ssl)
+logger.debug   request.forwarded?          # true (if running behind a reverse proxy)
+logger.debug   request.env                 # raw env hash handed in by Rack
 end
