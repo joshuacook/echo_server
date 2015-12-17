b72d12c727e5bf85f7b041b951f2c18ead58c3a5
Bobby Kearns <rkearns@invoca.com>
2015-12-17 10:45:46 -0800

    Added logging to real_time promo.

--- routes/real_time_routing.rb
+++ routes/real_time_routing.rb
@@ -4,7 +4,7 @@ class MyApp < Sinatra::Application
   get '/real_time_routing' do
     slim :real_time_routing
   end
-  
+
   post '/real_time_routing/bundle' do
     request.body.rewind
     request_payload = JSON.parse request.body.read
@@ -14,7 +14,7 @@ class MyApp < Sinatra::Application
     content_type :json
     { :destination_campaign_id => destination_campaign_id}.to_json
   end
-  
+
   post '/real_time_routing/bundificate' do
     request.body.rewind
     request_payload = JSON.parse request.body.read
@@ -23,8 +23,11 @@ class MyApp < Sinatra::Application
     content_type :json
     { :destination_campaign_id_from_network => destination_campaign_id}.to_json
   end
-  
+
   post '/real_time_routing/promo' do
+    request.body.rewind
+    request_payload = JSON.parse request.body.read
+    logger.debug request_payload
     destination_phone_numbers = ['+17074437062','+19072665145','+15132411010','+12076883210']
     destination_phone_number = destination_phone_numbers.sample
     content_type :json
